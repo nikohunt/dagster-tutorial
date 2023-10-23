c58b4e2265795ee46c45e1e04c65e1e828928073
Nikolas Hunt <nikolas.hunt@contino.io>
2023-10-23 11:30:18 +0100

    Use io manager for topstory_ids

--- tutorial/tutorial/assets.py
+++ tutorial/tutorial/assets.py
@@ -19,9 +19,8 @@ def topstory_ids() -> None:
     newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     top_new_story_ids = requests.get(newstories_url).json()[:100]
 
-    os.makedirs("data", exist_ok=True)
-    with open("data/topstory_ids.json", "w") as f:
-        json.dump(top_new_story_ids, f)
+    # return top_new_story_ids and the I/O manager will save it
+    return top_new_story_ids
 
 
 @asset(deps=[topstory_ids])
