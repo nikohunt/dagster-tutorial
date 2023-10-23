ed50782ed048ff36790896dd51d4ed4a3723b567
Nikolas Hunt <nikolas.hunt@contino.io>
2023-10-23 11:05:54 +0100

    Add job

--- tutorial/tutorial/__init__.py
+++ tutorial/tutorial/__init__.py
@@ -1,9 +1,17 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    AssetSelection,
+    Definitions,
+    define_asset_job,
+    load_assets_from_modules,
+)
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
-defs = Definitions(
-    assets=all_assets,
+# Addition: define a job that will materialize the assets
+hackernews_job = define_asset_job(
+    "hackernews_job", selection=AssetSelection.all()
 )
+
+defs = Definitions(assets=all_assets, jobs=[hackernews_job])
